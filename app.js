3be419d590835c5c79670d9622def9ae9fc5b087
A.J.K.G <athanasuisgadosey@gmail.com>
2022-06-30 04:24:14 +0000

    passport packages add

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const md5 = require('md5');
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const bcrypt = require('bcrypt');
+const saltRounds = 12;
 
 const app = express();
 
@@ -12,14 +17,34 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
+// Session for express initial configuration
+app.use(session({
+    secret: 'Our Worries', 
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser: true});
+mongoose.set('useCreateIndex', true);
 
 const userSchema = new mongoose.Schema ({
     email: String,
     password: String
 });
 
+// passportLocalMongoose config
+userSchema.plugin(passportLocalMongoose);
+
 const User = new mongoose.model('User', userSchema);
+
+// passportLocalMongoose configuration
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get('/', function(req, res){
     res.render('home');
@@ -33,43 +58,57 @@ app.get('/register', function(req, res){
     res.render('register');
 });
 
+app.get('/secrets', function(req, res){
+    if (req.isAuthenticated()){
+        res.render('secrets');
+    }
+    else{
+        res.redirect('/login');
+    }
+});
+
 // Register Route
 app.post('/register', function(req, res){
-    const newUser = new User ({
-        email: req.body.username,
-        password: md5(req.body.password)
+    User.register({username: req.body.username}, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            res.redirect('/register');
+        }
+        else{
+            passport.authenticate('local') (req, res, function(){
+                res.redirect('/secrets');
+            });
+        }
     });
 
-    newUser.save(function(err){
-        if (err){
-            console.log(err);
-        }
-        else {
-            res.render('secrets');
-        }
-    });
 });
 
 // Login Route
 app.post('/login', function(req, res){
-    const username = req.body.username;
-    const password = req.body.password;
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
 
-    User.findOne({email: username}, function(err, foundUser){
-        if (err){
+    req.login(user, function(err){
+        if(err){
             console.log(err);
         }
-        else {
-            if(foundUser){
-                if(foundUser.password === password){
-                    res.render('secrets');
-                }
-            }
+        else{
+            passport.authenticate('local') (req, res, function(){
+                res.redirect('/secrets');
+            });
         }
     });
 });
 
+// Logout route
+app.get('/logout', function(req, res){
+    req.logout();
+    res.redirect('/');
+});
 
+//this is shows the ports server
 app.listen(4000, function(){
     console.log('Ports run on 4000');
 });
---
+++ ls
@@ -0,0 +1,11 @@
+[33mcommit a0c8ede4599ea0f65db487ace764ff1baa2f6f3e[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m, [m[1;31morigin/master[m[33m)[m
+Author: A.J.K.G <athanasuisgadosey@gmail.com>
+Date:   Wed Jun 29 04:29:44 2022 +0000
+
+    second edit
+
+[33mcommit bacd5526c12000cdbe7ba77ae3c531dc343f5fa0[m
+Author: A.J.K.G <athanasuisgadosey@gmail.com>
+Date:   Wed Jun 29 04:25:14 2022 +0000
+
+    first hiddenWor..
